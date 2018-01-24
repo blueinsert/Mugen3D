5e4c3dfb80eac4126de780e486d224513266f4d2
bluebean <1401597377@qq.com>
2018-01-24 23:11:08 +0800

    new

---
+++ Assets/Scripts/Load/FightSceneLoader.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FightSceneLoader : MonoBehaviour {
+    public GameObject goLoading;
+    private bool isLoadingComplete = false;
+
+    private ClientGame clientGame;
+
+    IEnumerator CreateGame(PlayMode playMode, string p1CharacterName, string p2CharacterName, string stage)
+    {
+        GameObject gameGo = new GameObject();
+        gameGo.name = "ClientGame";
+        var clientGame = gameGo.AddComponent<ClientGame>();
+        clientGame.StartGame(p1CharacterName, p2CharacterName, stage, playMode);
+        this.clientGame = clientGame;
+        SetEnabled<Camera>(clientGame.gameObject, false);
+        SetEnabled<AudioListener>(clientGame.gameObject, false);
+        isLoadingComplete = true;
+        yield return null;
+    }
+
+    IEnumerator WaitForLoadingComplete(System.Action onComplete)
+    {
+        float timer = 0;
+        while (timer < 1 || isLoadingComplete == false)
+        {
+            timer += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        onComplete();
+        yield return null;
+    }
+
+    private void SetEnabled<T>(GameObject go, bool isEnabled) where T : Behaviour
+    {
+        foreach (T t in go.GetComponentsInChildren<T>())
+        {
+            t.enabled = isEnabled;
+        }
+    }
+
+    public void Loading(PlayMode playMode, string p1CharacterName, string p2CharacterName, string stage)
+    {
+        isLoadingComplete = false;
+        StartCoroutine(WaitForLoadingComplete(() => {
+            GameObject.Destroy(goLoading);
+            SetEnabled<Camera>(this.clientGame.gameObject, true);
+            SetEnabled<AudioListener>(this.clientGame.gameObject, true);
+        }));
+        StartCoroutine(CreateGame(playMode, p1CharacterName, p2CharacterName, stage));
+    }
+}
---
+++ Assets/Scripts/Load/SceneLoader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : Singleton<SceneLoader>  {
+
+    IEnumerator WaitForLoadingComplete(AsyncOperation oper, System.Action onComplete)
+    {
+        float timer = 0;
+        while (timer < 1 || oper.isDone == false)
+        {
+            timer += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        onComplete();
+        yield return null;
+    }
+
+    public  void LoadFightScene(PlayMode playMode, string p1CharacterName, string p2CharacterName, string stage) {
+       var oper = SceneManager.LoadSceneAsync("FightScene", LoadSceneMode.Single);
+       StartCoroutine(WaitForLoadingComplete(oper, () => {
+           var loader = GameObject.FindObjectOfType<FightSceneLoader>();
+           loader.Loading(playMode, p1CharacterName, p2CharacterName, stage);
+       }));
+       
+    }
+
+}
