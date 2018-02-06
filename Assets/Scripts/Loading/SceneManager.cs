14700b7e2ee61e96820c6e94d8268adbd30a16a6
bluebean <1401597377@qq.com>
2018-02-06 23:30:23 +0800

    new

--- Assets/Scripts/Loading/SceneManager.cs
+++ Assets/Scripts/Loading/SceneManager.cs
@@ -4,6 +4,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using FSoul.UI;
 
+public enum LoadType { 
+    Load,
+    Create,
+}
+
+public enum LoadMode
+{
+    Single,
+    Additive,
+}
+
 public class SceneManager : MonoBehaviour {
     private SceneManager() { }
     public static SceneManager Instance {get{return m_instance;}}
@@ -13,14 +24,14 @@ public class SceneManager : MonoBehaviour {
     {
         m_instance = this;
         DontDestroyOnLoad(this.gameObject);
-        foreach (var go in goScenes) {
+        foreach (var go in prefabScenes) {
             m_scenesPrefabs.Add(go.name, go);
         }
     }
 
     private Dictionary<string, GameObject> m_scenesIns = new Dictionary<string,GameObject>();
     private Dictionary<string, GameObject> m_scenesPrefabs = new Dictionary<string, GameObject>();
-    public List<GameObject> goScenes = new List<GameObject>();
+    public List<GameObject> prefabScenes = new List<GameObject>();
     public GameObject curScene;
 
     public void AddScene(string name, GameObject scene)
@@ -37,6 +48,15 @@ public class SceneManager : MonoBehaviour {
         }
     }
 
+    public void RemoveAllScene()
+    {
+        foreach (var kv in m_scenesIns)
+        {
+            GameObject.Destroy(kv.Value);
+        }
+        m_scenesIns.Clear();
+    }
+
     public void HideOtherScene(string curSceneName)
     {
         foreach (var kv in m_scenesIns)
@@ -48,29 +68,110 @@ public class SceneManager : MonoBehaviour {
         }
     }
 
+    public void HideAllScene()
+    {
+        foreach (var kv in m_scenesIns)
+        {
+            kv.Value.SetActive(false);
+        }
+    }
+
+    private void LoadScene(LoadType type, string sceneName, Action<GameObject> onBeginLoad, Action<GameObject> onFinishLoad, LoadMode mode = LoadMode.Single) {
+        if (mode == LoadMode.Single)
+        {
+            RemoveAllScene();
+        }
+        GameObject scene = null;
+        if (type == LoadType.Create)
+        {
+            scene = new GameObject(sceneName);
+        }
+        else if (type == LoadType.Load)
+        {
+            var prefab = m_scenesPrefabs[sceneName];
+            scene = GameObject.Instantiate(prefab);
+        }
+        if (onBeginLoad != null)
+        {
+            onBeginLoad(scene);
+        }
+        curScene = scene;
+        AddScene(sceneName, scene);
+        if (onFinishLoad != null)
+        {
+            onFinishLoad(scene);
+        }
+    }
+
+    IEnumerator DoLoadSceneAsync(LoadType type, string sceneName, Action<GameObject> onBeginLoad, Action<GameObject> onFinishLoad, LoadMode mode = LoadMode.Single) {
+        if (mode == LoadMode.Single)
+        {
+            RemoveAllScene();
+        }
+        GameObject scene = null;
+        if (type == LoadType.Create)
+        {
+            scene = new GameObject(sceneName);
+        }
+        else if (type == LoadType.Load)
+        {
+            var prefab = m_scenesPrefabs[sceneName];
+            scene = GameObject.Instantiate(prefab);
+        }
+        if (onBeginLoad != null)
+        {
+            onBeginLoad(scene);
+        }
+        curScene = scene;
+        AddScene(sceneName, scene);
+        yield return new WaitForSeconds(0.5f);
+        if (onFinishLoad != null)
+        {
+            onFinishLoad(scene);
+        }
+        yield return null;
+    }
+
+    private void LoadSceneAsync(LoadType type, string sceneName, Action<GameObject> onBeginLoad, Action<GameObject> onFinishLoad, LoadMode mode = LoadMode.Single)
+    {
+        StartCoroutine(DoLoadSceneAsync(type, sceneName, onBeginLoad, onFinishLoad, mode));
+    }
+
     public void LoadFightScene(PlayMode playMode, string p1CharacterName, string p2CharacterName, string stage) {
-        RemoveScene("Scene_MainMenu");
-
-        GameObject gameGo = new GameObject("Scene_Fight");
-        var clientGame = gameGo.AddComponent<ClientGame>();
-        clientGame.CreateGame(p1CharacterName, p1CharacterName, stage, playMode);
-        clientGame.StartGame();
-
-        curScene = gameGo;
-        AddScene("Scene_Fight", gameGo);
-        HideOtherScene("Scene_Fight");
-        
+        LoadScene(LoadType.Load, "Scene_Loading", null, null);
+        LoadSceneAsync(LoadType.Create, "Scene_Fight", 
+            (scene) => {
+                scene.SetActive(false);
+                var clientGame = scene.AddComponent<ClientGame>();
+                clientGame.CreateGame(p1CharacterName, p1CharacterName, stage, playMode);
+               
+            }, 
+            (scene) => {
+                RemoveScene("Scene_Loading");
+                scene.SetActive(true);
+                scene.GetComponent<ClientGame>().StartGame();
+            },
+            LoadMode.Additive
+        );       
     }
 
     public void LoadMainMenu()
     {
-        var prefab = m_scenesPrefabs["Scene_MainMenu"];
-        var goScene = GameObject.Instantiate(prefab);
-        UIManager.Instance.PushView("ViewMainMenu", GameObject.Find("UIRoot/Canvas/BaseGroup").transform);
-
-        curScene = goScene;
-        AddScene("Scene_MainMenu", goScene);
-        HideOtherScene("Scene_MainMenu");
+        LoadScene(LoadType.Load, "Scene_MainMenu",
+            (scene) => { 
+                UIManager.Instance.PushView("ViewMainMenu", GameObject.Find("UIRoot/Canvas/BaseGroup").transform); 
+            }, 
+            null
+        );
     }
 
+    /*
+    private void SetEnabled<T>(GameObject go, bool isEnabled) where T : Behaviour
+    {
+        foreach (T t in go.GetComponentsInChildren<T>())
+        {
+            t.enabled = isEnabled;
+        }
+    }
+    */
 }
